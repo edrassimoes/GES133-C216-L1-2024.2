5dcd0a1550c8704178e7fad9127a8f459e4f14fa
edrassimoes <edrassimoes@gmail.com>
2024-10-03 09:04:53 -0300

    atualizando backend

--- backend/main.py
+++ backend/main.py
@@ -1,43 +1,123 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
+from typing import List, Optional
+import time
 
-# Definindo a classe para o corpo da requisição
-class User(BaseModel):
-    name: str
+jogos = [
+    {"id": 1, "titulo": "The Legend of Zelda™: Echoes of Wisdom", "desenvolvedor": "Nintendo", "quantidade": 20, "preco": 299.0},
+    {"id": 2, "titulo": "ASTRO BOT", "desenvolvedor": "Team Asobi", "quantidade": 15, "preco": 299.9},
+    {"id": 3, "titulo": "SILENT HILL 2", "desenvolvedor": "Konami", "quantidade": 10, "preco": 349.9},
+]
 
 app = FastAPI()
 
-# Método GET - Rota raiz
-@app.get("/")
-async def hello_world():
-    return {"message": "Hello, FastAPI!"}
+class Jogo(BaseModel):
+    id: Optional[int] = None
+    titulo: str
+    desenvolvedor: str
+    quantidade: int
+    preco: float
 
-# Método GET - Retorna uma saudação usando um query parameter
-@app.get("/api/v1/hello")
-async def hello_name_via_query(name: str):
-    return {"message": f"Hello {name}"}
 
-# Método GET - Retorna uma saudação usando um path parameter
-@app.get("/api/v1/hello/{name}")
-async def hello_name_via_path(name: str):
-    return {"message": f"Hello {name}"}
+class VendaJogo(BaseModel):
+    quantidade: int
 
-# Método POST - Retorna uma saudação utilizando o nome do usuário no corpo da requisição
-@app.post("/api/v1/hello")
-async def hello_name(user: User):
-    return {"message": f"Hello {user.name}"}
 
-# Método PUT - Atualiza um recurso com o nome do usuário
-@app.put("/api/v1/update")
-async def put_update(user: User):
-    return {"message": f"Recurso atualizado com o nome: {user.name}"}
+class AtualizarJogo(BaseModel):
+    titulo: Optional[str] = None
+    desenvolvedor: Optional[str] = None
+    quantidade: Optional[int] = None
+    preco: Optional[float] = None
 
-# Método DELETE - Deleta um recurso pelo nome do usuário passado via query parameter
-@app.delete("/api/v1/delete")
-async def delete_name(name: str):
-    return {"message": f"Recurso deletado com o nome: {name}"}
 
-# Método PATCH - Aplica uma modificação parcial ao recurso com o nome do usuário no corpo da requisição
-@app.patch("/api/v1/patch")
-async def patch_name(user: User):
-    return {"message": f"Modificação parcial aplicada ao recurso com o nome: {user.name}"}
+def gerar_proximo_id():
+    if jogos:
+        return max(jogo['id'] for jogo in jogos) + 1
+    else:
+        return 1
+
+
+def buscar_jogo_por_id(jogo_id: int):
+    for jogo in jogos:
+        if jogo["id"] == jogo_id:
+            return jogo
+    return None
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    print(f"Path: {request.url.path}, Method: {request.method}, Process Time: {process_time:.4f}s")
+    return response
+
+@app.post("/api/v1/jogos/", status_code=201)
+def adicionar_jogo(jogo: Jogo):
+    for j in jogos:
+        if j["desenvolvedor"].lower() == jogo.desenvolvedor.lower() and j["titulo"].lower() == jogo.titulo.lower():
+            raise HTTPException(status_code=400, detail="O jogo já se encontra cadastrado no sistema.")
+
+    novo_jogo = jogo.dict()
+    novo_jogo['id'] = gerar_proximo_id()
+
+    jogos.append(novo_jogo)
+    return {"message": "Jogo cadastrado com sucesso!", "jogo": novo_jogo}
+
+@app.get("/api/v1/jogos/", response_model=List[Jogo])
+def listar_jogos():
+    return jogos
+
+@app.get("/api/v1/jogos/{jogo_id}")
+def listar_jogo_por_id(jogo_id: int):
+    jogo = buscar_jogo_por_id(jogo_id)
+    if jogo is None:
+        raise HTTPException(status_code=404, detail="Jogo não encontrado.")
+    return jogo
+
+@app.put("/api/v1/jogos/{jogo_id}/vender/")
+def vender_jogo(jogo_id: int, venda: VendaJogo):
+    jogo = buscar_jogo_por_id(jogo_id)
+
+    if jogo is None:
+        raise HTTPException(status_code=404, detail="Jogo não encontrado.")
+
+    if jogo["quantidade"] < venda.quantidade:
+        raise HTTPException(status_code=400, detail="Quantidade insuficiente no estoque.")
+
+    jogo["quantidade"] -= venda.quantidade
+    return {"message": "Venda realizada com sucesso!", "jogo": jogo}
+
+@app.patch("/api/v1/jogos/{jogo_id}")
+def atualizar_jogo(jogo_id: int, jogo_atualizacao: AtualizarJogo):
+    jogo = buscar_jogo_por_id(jogo_id)
+    if jogo is None:
+        raise HTTPException(status_code=404, detail="Jogo não encontrado.")
+
+    if jogo_atualizacao.titulo is not None:
+        jogo["titulo"] = jogo_atualizacao.titulo
+    if jogo_atualizacao.desenvolvedor is not None:
+        jogo["desenvolvedor"] = jogo_atualizacao.desenvolvedor
+    if jogo_atualizacao.quantidade is not None:
+        jogo["quantidade"] = jogo_atualizacao.quantidade
+    if jogo_atualizacao.preco is not None:
+        jogo["preco"] = jogo_atualizacao.preco
+
+    return {"message": "Jogo atualizado com sucesso!", "jogo": jogo}
+
+@app.delete("/api/v1/jogos/{jogo_id}")
+def remover_jogo(jogo_id: int):
+    for i, jogo in enumerate(jogos):
+        if jogo["id"] == jogo_id:
+            del jogos[i]
+            return {"message": "Jogo removido com sucesso!"}
+
+@app.delete("/api/v1/jogos/")
+def resetar_jogos():
+    global jogos
+    jogos = [
+        {"id": 1, "titulo": "The Legend of Zelda™: Echoes of Wisdom", "desenvolvedor": "Nintendo", "quantidade": 20,
+         "preco": 299.0},
+        {"id": 2, "titulo": "ASTRO BOT", "desenvolvedor": "Team Asobi", "quantidade": 15, "preco": 299.9},
+        {"id": 3, "titulo": "SILENT HILL 2", "desenvolvedor": "Konami", "quantidade": 10, "preco": 349.9},
+    ]
+    return {"message": "Repositorio limpo com sucesso!", "jogos": jogos}
